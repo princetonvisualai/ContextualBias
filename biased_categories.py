3b91bc6a7fd28cbfd9b213879cecbafcee06869a
Sharon Zhang <zsharonx@gmail.com>
2020-11-05 16:26:13 -0500

    add section to compute biases of 20 paper categories

--- biased_categories.py
+++ biased_categories.py
@@ -27,6 +27,20 @@ def bias(b, z, imgs_b, imgs_z, co_occur, scores_val):
 
     return bias_val
 
+def get_pair_bias(b, z, scores_val, CO_OCCUR_PERCENT=0.1):
+    if b == z:
+        print('Same category, exiting')
+        return 0.0
+
+    imgs_b = set(label_to_img[b]) # List of images containing b
+    num_imgs_b = len(imgs_b)
+
+    imgs_z = set(label_to_img[z])
+    co_occur = imgs_b.intersection(imgs_z)
+    if len(co_occur)/len(imgs_b) < CO_OCCUR_PERCENT:
+        print('WARNING: Categories {} and {} co-occur infrequently'.format(b, z))
+    return bias(b, z, imgs_b, imgs_z, co_occur, scores_val)
+
 # Load files
 labels_val = pickle.load(open('/n/fs/context-scr/labels_train_20.pkl', 'rb'))
 scores_val = pickle.load(open('/n/fs/context-scr/scores_train_20.pkl', 'rb'))
@@ -45,44 +59,75 @@ for img_name in labels_val.keys():
         label = int(idx[0])
         label_to_img[label].append(img_name)
 
-# Calculate bias and get the most biased category for b
-num_categs = 171
-biased_pairs = np.zeros((num_categs, 3)) # 2d array with columns b, c, bias
+# Compute biases for 20 categories in paper
+original_biased_pairs = [
+    ['cup', 'dining table'],
+    ['wine glass', 'person'],
+    ['handbag', 'person'],
+    ['apple', 'fruit'],
+    ['car', 'road'],
+    ['bus', 'road'],
+    ['potted plant', 'vase'],
+    ['spoon', 'bowl'],
+    ['microwave', 'oven'],
+    ['keyboard', 'mouse'],
+    ['skis', 'person'],
+    ['clock', 'building-other'],
+    ['sports ball', 'person'],
+    ['remote', 'person'],
+    ['snowboard', 'person'],
+    ['toaster', 'ceiling-other'],
+    ['hair drier', 'towel'],
+    ['tennis racket', 'person'],
+    ['skateboard', 'person'],
+    ['baseball glove', 'person']
+]
+if True:
+    for pair in original_biased_pairs:
+        b = humanlabels_to_onehot[pair[0]]
+        z = humanlabels_to_onehot[pair[1]]
+        pair_bias = get_pair_bias(b, z, scores_val)
+        print('({}, {}): {}'.format(pair[0], pair[1], pair_bias))
 
-for b in range(num_categs):
+# Compute top biased pair for each category and record top 20 most biased category pairs
+if False:
+    # Calculate bias and get the most biased category for b
+    num_categs = 171
+    biased_pairs = np.zeros((num_categs, 3)) # 2d array with columns b, c, bias
 
-    imgs_b = set(label_to_img[b]) # List of images containing b
-    num_imgs_b = len(imgs_b) # Number of images containing b
+    for b in range(num_categs):
+        imgs_b = set(label_to_img[b]) # List of images containing b
+        num_imgs_b = len(imgs_b) # Number of images containing b
 
-    b_human = list(humanlabels_to_onehot.keys())[list(humanlabels_to_onehot.values()).index(b)]
-    print('\n{}({}) appears in {}/{} images'.format(b_human, b, num_imgs_b, len(labels_val)))
+        b_human = list(humanlabels_to_onehot.keys())[list(humanlabels_to_onehot.values()).index(b)]
+        print('\n{}({}) appears in {}/{} images'.format(b_human, b, num_imgs_b, len(labels_val)))
 
-    # Calculate bias values for categories that co-occur with b more than 10% of the times
-    biases_b = np.zeros(num_categs) # Array containing bias value of (b, z)
-    for z in range(num_categs):
-        if z == b:
-            continue
+        # Calculate bias values for categories that co-occur with b more than 10% of the times
+        biases_b = np.zeros(num_categs) # Array containing bias value of (b, z)
+        for z in range(num_categs):
+            if z == b:
+                continue
 
-        imgs_z = set(label_to_img[z])
-        co_occur = imgs_b.intersection(imgs_z)
-        if len(co_occur)/len(imgs_b) > CO_OCCUR_PERCENT:
-            biases_b[z] = bias(b, z, imgs_b, imgs_z, co_occur, scores_val)
+            imgs_z = set(label_to_img[z])
+            co_occur = imgs_b.intersection(imgs_z)
+            if len(co_occur)/len(imgs_b) > CO_OCCUR_PERCENT:
+                biases_b[z] = bias(b, z, imgs_b, imgs_z, co_occur, scores_val)
 
-    # Identify c that has the highest bias for b
-    if np.sum(biases_b) != 0:
-        c = np.argmax(biases_b)
-        biased_pairs[b] = [b, c, biases_b[c]]
+        # Identify c that has the highest bias for b
+        if np.sum(biases_b) != 0:
+            c = np.argmax(biases_b)
+            biased_pairs[b] = [b, c, biases_b[c]]
 
-    c_human = list(humanlabels_to_onehot.keys())[list(humanlabels_to_onehot.values()).index(c)]
-    print('b {}({}), c {}({}), bias {}'.format(b_human, b, c_human, c, biases_b[c]))
+        c_human = list(humanlabels_to_onehot.keys())[list(humanlabels_to_onehot.values()).index(c)]
+        print('b {}({}), c {}({}), bias {}'.format(b_human, b, c_human, c, biases_b[c]))
 
 
-# Get top 20 biased categories
-top_20_idx = np.argsort(biased_pairs[:,2])[-20:]
-top_20 = []
-for i in top_20_idx:
-    top_20.append([onehot_to_humanlabels[int(biased_pairs[i,0])], onehot_to_humanlabels[int(biased_pairs[i,1])], biased_pairs[i,2]])
+    # Get top 20 biased categories
+    top_20_idx = np.argsort(biased_pairs[:,2])[-20:]
+    top_20 = []
+    for i in top_20_idx:
+        top_20.append([onehot_to_humanlabels[int(biased_pairs[i,0])], onehot_to_humanlabels[int(biased_pairs[i,1])], biased_pairs[i,2]])
 
-data = {'top_20': top_20, 'biased_pairs': biased_pairs}
-with open('biased_categories.pkl', 'wb') as handle:
-    pickle.dump(data, handle)
+    data = {'top_20': top_20, 'biased_pairs': biased_pairs}
+    with open('biased_categories.pkl', 'wb') as handle:
+        pickle.dump(data, handle)
