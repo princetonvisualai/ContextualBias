c76535d56595e5d701154e14b48184f20d134692
Sharon Zhang <zsharonx@gmail.com>
2020-11-20 18:17:23 -0500

    add UnRel data process

--- UnRel/data_process.py
+++ UnRel/data_process.py
@@ -9,22 +9,11 @@ from scipy.io import loadmat
 # Data directory
 datadir = '/n/fs/visualai-scr/Data/UnRel'
 
-# Create human labels (only 3 shared with biased categories in COCOStuff)
-humanlabels = {
-    1: 'car',
-    2: 'bus',
-    3: 'skateboard'
-}
+# Load COCOStuff labels
+humanlabels_to_onehot = pickle.load(open('/n/fs/context-scr/COCOStuff/humanlabels_to_onehot.pkl', 'rb'))
 
-# Create a dictionary that maps human-readable labels to [0-2]
-humanlabels_to_onehot = {
-    'car': 0,
-    'bus': 1,
-    'skateboard': 2
-}
-with open('humanlabels_to_onehot.pkl', 'wb+') as handle:
-    pickle.dump(humanlabels_to_onehot, handle)
-print('Saved humanlabels_to_onehot.pkl')
+# Shared classes between COCOStuff biased and UnRel
+shared_classes = ['car', 'bus', 'skateboard']
 
 # Create a list of image file names (test)
 start_time = time.time()
@@ -35,7 +24,7 @@ if True:
     
     # Process image if it contains a label in COCOStuff
     for i in range(annotations.shape[0]):
-        filename = annotations[i][0][0][0][0][0]
+        filename = '/n/fs/visualai-scr/Data/UnRel/images/{}'.format(annotations[i][0][0][0][0][0])
         annotation = annotations[i][0][0][0][3] # loadmat creates a deeply nested array
         keep = False
         label = []
@@ -43,17 +32,18 @@ if True:
             s = obj[0][0][0][0][0] # loadmat creates a deeply nested array
             
             # Keep the image and create a label if it shares classes with COCOStuff
-            if s in humanlabels_to_onehot:
+            if s in shared_classes and humanlabels_to_onehot[s] not in label:
                 keep = True
                 label.append(humanlabels_to_onehot[s])
+        
         if keep:
-            label_onehot = torch.nn.functional.one_hot(torch.LongTensor(label), num_classes=3)
+            label_onehot = torch.nn.functional.one_hot(torch.LongTensor(label), num_classes=171)
             label_onehot = label_onehot.sum(dim=0).float()
             labels[filename] = label_onehot
 
         count += 1
 
-    print('Finished processing {} test labels'.format(len(labels)))
-    with open('labels_test.pkl', 'wb+') as handle:
+    print('Finished processing {} val labels'.format(len(labels)))
+    with open('labels_val.pkl', 'wb+') as handle:
        pickle.dump(labels, handle)
 
