b05f226e26f7346335f8880827b1997a8501b172
Sunnie S. Y. Kim <sunnie.suhyoung.kim@gmail.com>
2020-11-26 12:05:59 -0800

    updated job script

--- job.sh
+++ job.sh
@@ -4,17 +4,20 @@
 #SBATCH --ntasks-per-node 1    # number of tasks per node
 #SBATCH --exclude=node718      # exclude the node that often causes errors
 #SBATCH -A visualai            # specify which group of nodes to use
-#SBATCH --mem-per-cpu=4G      # memory per cpu-core (4G default)
-#SBATCH --gres=gpu:1           # number of GPUs requested
-#SBATCH -t 10:00:00            # time requested in hour:minute:second
+#SBATCH --mem-per-cpu=4G       # memory per cpu-core (4G default)
+#SBATCH --gres=gpu:rtx_3090:1  # number of GPUs requested
+#SBATCH -t 100:00:00           # time requested in hour:minute:second
 
-#source /n/fs/context-scr/context/bin/activate
-source /n/fs/visualai-scr/sunnie/basic/bin/activate
+source /n/fs/context-scr/context/bin/activate # for RTX3090
+# source /n/fs/visualai-scr/sunnie/basic/bin/activate # for non-RTX3090
 
-#python train.py --model baseline --batchsize 200 --outdir save
-#python train.py --model cam --batchsize 50 --outdir save
-#python train.py --model featuresplit --batchsize 100 --outdir save
-#python train.py --model removeclabels --batchsize 100 --outdir save
-#python train.py --model removecimages --batchsize 100 --outdir save
-#python train.py --model negativepenalty --batchsize 100 --outdir save
-python train.py --model featuresplit --batchsize 100 --outdir save
+python train.py --model baseline --batchsize 200 --outdir save
+
+# python train.py --model removeclabels --batchsize 200 --outdir save
+# python train.py --model removecimages --batchsize 200 --outdir save
+# python train.py --model negativepenalty --batchsize 200 --outdir save
+# python train.py --model classbalancing --batchsize 200 --outdir save
+
+# python train.py --model cam --batchsize 50 --outdir save \
+#   --pretrainedpath /n/fs/context-scr/COCOStuff/save/stage1/stage1_4.pth
+# python train.py --model featuresplit --batchsize 200 --outdir save
