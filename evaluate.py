259bc9a9659f7f71ca86a0f6f7461838a7a63a52
Sharon Zhang <zsharonx@gmail.com>
2020-12-01 12:19:58 -0500

    add argparse params to test hidden layer size

--- evaluate.py
+++ evaluate.py
@@ -13,6 +13,7 @@ parser.add_argument('--modelpath', type=str, default=None)
 parser.add_argument('--labels', type=str, default='/n/fs/context-scr/COCOStuff/labels_val.pkl')
 parser.add_argument('--batchsize', type=int, default=200)
 parser.add_argument('--nclasses', type=int, default=171)
+parser.add_argument('--hs', type=int, default=2048)
 parser.add_argument('--splitbiased', type=bool, default=False)
 parser.add_argument('--device', default=torch.device('cuda'))
 parser.add_argument('--dtype', default=torch.float32)
@@ -30,7 +31,7 @@ onehot_to_humanlabels = dict((y,x) for x,y in humanlabels_to_onehot.items())
 valset = create_dataset(arg['dataset'], arg['labels'], biased_classes_mapped, B=arg['batchsize'], train=False)
 
 # Load model
-Classifier = multilabel_classifier(arg['device'], arg['dtype'], arg['nclasses'], arg['modelpath'])
+Classifier = multilabel_classifier(arg['device'], arg['dtype'], arg['nclasses'], arg['modelpath'], hidden_size=arg['hs'])
 
 # Do inference with the model
 labels_list, scores_list, val_loss_list = Classifier.test(valset)
@@ -75,6 +76,6 @@ for k in range(len(biased_classes_list)):
     # Record and print
     cooccur_AP_list.append(cooccur_AP)
     exclusive_AP_list.append(exclusive_AP)
-    print('{:>15} - {:>15}: exclusive {:.2f}, co-occur {:.2f}'.format(onehot_to_humanlabels[b], onehot_to_humanlabels[c], exclusive_AP*100., cooccur_AP*100.))
+    print('{:>10} - {:>10}: exclusive {:5.2f}, co-occur {:5.2f}'.format(onehot_to_humanlabels[b], onehot_to_humanlabels[c], exclusive_AP*100., cooccur_AP*100.))
 
 print('\nMean: exclusive {:.2f}, co-occur {:.2f}'.format(np.mean(exclusive_AP_list)*100., np.mean(cooccur_AP_list)*100.))
