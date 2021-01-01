18b0d78d6a92abdb272512bb3f2a384169b293e5
Sharon Zhang <zsharonx@gmail.com>
2021-01-01 09:18:59 -0500

    update job examples

--- job.sh
+++ job.sh
@@ -5,7 +5,7 @@
 #SBATCH --exclude=node718      # exclude the node that often causes errors
 #SBATCH -A visualai            # specify which group of nodes to use
 #SBATCH --mem-per-cpu=4G       # memory per cpu-core (4G default)
-#SBATCH --gres=gpu:rtx_3090:1  # number of GPUs requested
+#SBATCH --gres=gpu:rtx_3090:2  # number of GPUs requested
 #SBATCH -t 16:00:00            # time requested in hour:minute:second
 
 source /n/fs/context-scr/context/bin/activate # for RTX3090
@@ -36,17 +36,24 @@ source /n/fs/context-scr/context/bin/activate # for RTX3090
 
 
 ### AwA
-python train.py --dataset AwA --model featuresplit --nepoch 20 --nclasses 85 \
-  --lr 0.1 --wd 0.0001 \
+#python train.py --dataset AwA --model featuresplit --nepoch 20 --nclasses 85 \
+#  --lr 0.01 --wd 0.0001 --drop 10 \
+#  --test_batchsize 150 --train_batchsize 200 \
+#  --labels_train /n/fs/context-scr/AwA/labels_train.pkl \
+#  --labels_test /n/fs/context-scr/AwA/labels_val.pkl \
+#  --outdir AwA/save/featuresplit_256
+
+python train.py --dataset AwA --model attribdecorr --nepoch 20 --nclasses 85 \
+  --lr 0.01 --wd 0.0001 --drop 10 --compshare_lambda 0.01 \
   --test_batchsize 150 --train_batchsize 200 \
   --labels_train /n/fs/context-scr/AwA/labels_train.pkl \
-  --labels_val /n/fs/context-scr/AwA/labels_val.pkl \
-  --pretrainedpath AwA/save/baseline_reducedlr_RandomResize224/baseline/model_19.pth \
-  --outdir AwA/save/featuresplit_2
+  --labels_test /n/fs/context-scr/AwA/labels_val.pkl \
+  --pretrainedpath AwA/save/baseline/model_19.pth \
+  --outdir AwA/save
 
 ### DeepFashion
 #python train.py --dataset DeepFashion --model baseline --nepoch 50 --nclasses 250 \
-#  --lr 0.1 --test_batchsize 140 \
+#  --lr 0.1 --test_batchsize 140 --drop 30 \
 #  --labels_train /n/fs/context-scr/DeepFashion/labels_train.pkl \
-#  --labels_val /n/fs/context-scr/DeepFashion/labels_val.pkl \
-#  --outdir DeepFashion/save/baseline_epoch20drop_RandomResize224
+#  --labels_test /n/fs/context-scr/DeepFashion/labels_val.pkl \
+#  --outdir DeepFashion/save
