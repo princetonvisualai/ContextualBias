ed566dc3d8e26d26d07e0e1c8f2492b9852cf9e3
Sharon Zhang <zsharonx@gmail.com>
2021-01-03 14:30:14 -0500

    reformat argparse options to mirror training options

--- evaluate.py
+++ evaluate.py
@@ -7,18 +7,36 @@ from sklearn.metrics import average_precision_score
 from classifier import multilabel_classifier
 from load_data import *
 
+def recall3(labels_list, scores_list, k):
+    '''
+    Top-3 recall evaluation (used for DeepFashion)
+    '''
+    num_imgs = np.sum(labels_list)
+    image_scores = scores_list[labels_list.astype(bool)]
+    
+    # Get top 3 labels (index) from images that contain label k                                                                        
+    if len(image_scores.shape)==1:
+        top3_labels = image_scores.argsort()[-3:]
+    else:
+        top3_labels = image_scores.argsort()[:, -3:]
+    num_intop3 = np.sum(top3_labels==k)
+
+    # Recall@3 is num_intop3/num_imgs                                                                                             
+    recall = num_intop3 / num_imgs if num_imgs != 0 else 0
+    return recall
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str)
+parser.add_argument('--model', type=str, default='baseline')
 parser.add_argument('--modelpath', type=str, default=None)
 parser.add_argument('--labels', type=str, default='/n/fs/context-scr/COCOStuff/labels_val.pkl')
 parser.add_argument('--batchsize', type=int, default=170)
 parser.add_argument('--nclasses', type=int, default=171)
 parser.add_argument('--hs', type=int, default=2048)
-parser.add_argument('--splitbiased', type=bool, default=False)
 parser.add_argument('--device', default=torch.device('cuda'))
 parser.add_argument('--dtype', default=torch.float32)
 arg = vars(parser.parse_args())
-if arg['splitbiased']:
+if arg['model'] == 'splitbiased':
     arg['nclasses'] = arg['nclasses'] + 20
 print('\n', arg, '\n')
 
@@ -30,32 +48,30 @@ humanlabels_to_onehot = pickle.load(open('/n/fs/context-scr/{}/humanlabels_to_on
 onehot_to_humanlabels = dict((y,x) for x,y in humanlabels_to_onehot.items())
 
 # Create dataloader
-valset = create_dataset(arg['dataset'], arg['labels'], biased_classes_mapped, B=arg['batchsize'], train=False, splitbiased=arg['splitbiased'])
+testset = create_dataset(arg['dataset'], arg['labels'], biased_classes_mapped, B=arg['batchsize'], train=False, splitbiased=(arg['model']=='splitbiased'))
 
 # Load model
 classifier = multilabel_classifier(arg['device'], arg['dtype'], arg['nclasses'], arg['modelpath'], hidden_size=arg['hs'])
 
 # Do inference with the model
-labels_list, scores_list, val_loss_list = classifier.test(valset)
+if arg['model'] in ['baseline', 'removeclabels', 'removecimages', 'splitbiased', 'cam', 'featuresplit']:
+    labels_list, scores_list, val_loss_list = classifier.test(testset)
+elif arg['model'] == 'negativepenalty':
+    labels_list, scores_list, val_loss_list = classifier.test_negativepenalty(testset, biased_classes_mapped, penalty=10)
+elif arg['model'] == 'classbalancing':
+    labels_list, scores_list, val_loss_list = classifier.test_classbalancing(testset, biased_classes_mapped, weight)
+elif arg['model'] == 'weighted':
+    labels_list, scores_list, test_loss_list = classifier.test_weighted(testset, biased_classes_mapped, weight=10)
+elif arg['model'] == 'attribdecorr':
+    labels_list, scores_list, test_loss_list = classifier.test_attribdecorr(testset, pretrained_net, biased_classes_mapped, pretrained_features)
+else:
+    print('Unknown model type: {}'.format(arg['model']))
+    labels_list = None
+    scores_list = None
+    test_loss_list = None
 
 # Calculate and record mAP
 APs = []
-
-def recall3(labels_list, scores_list, k):
-    num_imgs = np.sum(labels_list)
-    #print(labels_list[:,k].astype(int))
-    image_scores = scores_list[labels_list.astype(bool)]
-    #print(image_scores.shape)
-    # get top 3 labels (index) from images that contain label k                                                                        
-    if len(image_scores.shape)==1:
-        top3_labels = image_scores.argsort()[-3:]
-    else:
-        top3_labels = image_scores.argsort()[:, -3:]
-    num_intop3 = np.sum(top3_labels==k)
-    # Recall@3 is num_intop3/num_imgs                                                                                             
-    recall = num_intop3/num_imgs if num_imgs!=0 else 0
-    return recall
-
 
 if arg['dataset'] == 'DeepFashion':
     for k in range(arg['nclasses']):
@@ -75,29 +91,23 @@ if arg['dataset'] == 'COCOStuff':
 exclusive_AP_list = []
 cooccur_AP_list = []
 biased_classes_list = sorted(list(biased_classes_mapped.keys()))
-#print('biased classes list: ', biased_classes_list)
-#print('biased classes list[0]: ', biased_classes_list[0])
 
 for k in range(len(biased_classes_list)):
     b = biased_classes_list[k]
     c = biased_classes_mapped[b]
 
     # Categorize the images into co-occur/exclusive/other
-    if arg['splitbiased']:
+    if arg['model'] == 'splitbiased':
         cooccur = (labels_list[:,arg['nclasses']+k-20]==1)
         print(len(np.where(cooccur==True)[0]))
         exclusive = (labels_list[:,b]==1)
     else:
-        #print('b:', b)
-        #print(labels_list.shape)
-        #print(labels_list[:,b].shape)
-        #print(labels_list[:,c].shape)
         cooccur = (labels_list[:,b]==1) & (labels_list[:,c]==1)
         exclusive = (labels_list[:,b]==1) & (labels_list[:,c]==0)
     other = (~exclusive) & (~cooccur)
 
     # Calculate AP for co-occur/exclusive sets
-    if arg['splitbiased']:
+    if arg['model'] == 'splitbiased':
         cooccur_AP = average_precision_score(labels_list[cooccur+other, arg['nclasses']+k-20],
             scores_list[cooccur+other, arg['nclasses']+k-20])
     else:
