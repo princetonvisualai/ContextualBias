fe97879c824fa467be14c88ab849d1930d524a02
Sharon Zhang <zsharonx@gmail.com>
2021-01-07 10:32:56 -0500

    add support for AwA

--- split_80_20.py
+++ split_80_20.py
@@ -1,10 +1,13 @@
 import pickle
 import numpy as np
+import sys
 
-# Load the processed COCO-train labels
-labels_train = pickle.load(open('labels_train.pkl', 'rb'))
+dataset = sys.argv[1]
 
-# Do a 80-20 split of COCO-train
+# Load the processed train labels
+labels_train = pickle.load(open('/n/fs/context-scr/{}/labels_train.pkl'.format(dataset), 'rb'))
+
+# Do a 80-20 split of train
 N = len(list(labels_train.keys()))
 N_80 = int(N*0.8)
 
@@ -19,7 +22,7 @@ keys_20 = np.delete(keys, inds_80)
 labels_train_80 = {k: labels_train[k] for k in keys_80}
 labels_train_20 = {k: labels_train[k] for k in keys_20}
 
-with open('labels_train_80.pkl', 'wb') as handle:
+with open('/n/fs/context-scr/{}/labels_train_80.pkl'.format(dataset), 'wb') as handle:
     pickle.dump(labels_train_80, handle, protocol=4)
-with open('labels_train_20.pkl', 'wb') as handle:
+with open('/n/fs/context-scr/{}/labels_train_20.pkl'.format(dataset), 'wb') as handle:
     pickle.dump(labels_train_20, handle, protocol=4)
