379319e7188aeb965ad67b411bf6b832c591fd43
Sunnie S. Y. Kim <sunnie.suhyoung.kim@gmail.com>
2021-01-23 21:26:32 -0800

    process all 1,071 images

--- UnRel/data_process.py
+++ UnRel/data_process.py
@@ -21,29 +21,22 @@ annotations = loadmat('/n/fs/visualai-scr/Data/UnRel/annotations.mat')['annotati
 if True:
     count = 0
     labels = {}
-    
+
     # Process image if it contains a label in COCOStuff
     for i in range(annotations.shape[0]):
         filename = '/n/fs/visualai-scr/Data/UnRel/images/{}'.format(annotations[i][0][0][0][0][0])
         annotation = annotations[i][0][0][0][3] # loadmat creates a deeply nested array
-        keep = False
         label = []
         for obj in annotation:
             s = obj[0][0][0][0][0] # loadmat creates a deeply nested array
-            
-            # Keep the image and create a label if it shares classes with COCOStuff
-            if s in shared_classes and humanlabels_to_onehot[s] not in label:
-                keep = True
-                label.append(humanlabels_to_onehot[s])
-        
-        if keep:
-            label_onehot = torch.nn.functional.one_hot(torch.LongTensor(label), num_classes=171)
-            label_onehot = label_onehot.sum(dim=0).float()
-            labels[filename] = label_onehot
+            label.append(humanlabels_to_onehot[s])
+
+        label_onehot = torch.nn.functional.one_hot(torch.LongTensor(label), num_classes=171)
+        label_onehot = label_onehot.sum(dim=0).float()
+        labels[filename] = label_onehot
 
         count += 1
 
-    print('Finished processing {} val labels'.format(len(labels)))
-    with open('labels_val.pkl', 'wb+') as handle:
+    print('Finished processing {} UnRel labels'.format(len(labels)))
+    with open('labels_test.pkl', 'wb+') as handle:
        pickle.dump(labels, handle)
-
